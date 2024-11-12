bb3a9c0639a365029b0a78426faf3eebf037c39a
Bhavesh Budharaju <122691714+Bhavesh2k4@users.noreply.github.com>
2024-11-12 18:56:15 +0530

    Update configdb.py

--- configdb.py
+++ configdb.py
@@ -1,16 +1,26 @@
 import os
 from dotenv import load_dotenv
+import pymysql
 
-# Load environment variables
+# Load environment variables from .env file
 load_dotenv()
-password=os.getenv('PASSWORD')
-username="root"
-hostname=os.getenv('HOST')
-database=os.getenv('DATABASE')
+
+# Retrieve environment variables
+
+password = os.getenv('PASSWORD')
+database = os.getenv('DATABASE')
+port = int(os.getenv('PORT', 3306))  # Default to 3306 if PORT is not specified
+
+# Database configuration
 DB_CONFIG = {
-    'host': hostname,
-    'user': username,
+    'host': "mysql-sqlhire-sqlhire.d.aivencloud.com",
+    'user': "avnadmin",
     'password': password,
     'database': database,
-    # 'port' : <port_no> if a specific port is needed 
+    'port': port,
+    'charset': "utf8mb4",
+    'connect_timeout': 10,
+    'read_timeout': 10,
+    'write_timeout': 10,
+    'cursorclass': pymysql.cursors.DictCursor
 }
